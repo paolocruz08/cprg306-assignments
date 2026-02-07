816d71a499a0b0fd0b483246bcc4dd86684312e3
Paolo Cruz <paolo.cruz24@gmail.com>
2026-02-06 23:38:31 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,8 +6,8 @@ export default function Home() {
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <section>
         <Link href="/week-2" className="hover:underline">Go to Week 2 -{'>'} </Link> 
-        <Link href="/week-3" className="hover:underline">Go to Week 3 -{'>'}</Link>
-        <Link href="/week-4" className="hover:underline">Go to Week 4 -{'>'}</Link>
+        <Link href="/week-3" className="hover:underline">Go to Week 3 -{'>'} </Link>
+        <Link href="/week-4" className="hover:underline">Go to Week 4 -{'>'} </Link>
       </section>
     </main>
   );
